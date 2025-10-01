a6391a97844c742d5deca92d2c66785c326b4618
Guilherme Andrade <guiandradeemail@gmail.com>
2025-09-30 22:28:27 -0300

    feat: add num moves in graphics

--- tests/benchmark.cpp
+++ tests/benchmark.cpp
@@ -19,17 +19,21 @@ int main() {
     cubo.init();
     Solver solver;
 
+    // Cabeçalho expandido com colunas de "N. Moves"
     cout << left 
          << setw(8) << "Index"
          << setw(10) << "Shuffles"
          << right 
          << setw(12) << "BFS Time"
+         << setw(12) << "BFS Moves"
          << setw(12) << "BFS States"
          << setw(12) << "DFS Time"
+         << setw(12) << "DFS Moves"
          << setw(12) << "DFS States"
          << setw(12) << "A* Time"
+         << setw(12) << "A* Moves"
          << setw(12) << "A* States" << endl;
-    cout << string(88, '-') << endl;
+    cout << string(135, '-') << endl;
 
     int shuffle_max = 16;
 
@@ -53,7 +57,9 @@ int main() {
         solver.bfs(copy, path, &num_tries); 
         auto end_bfs = std::chrono::high_resolution_clock::now();
         auto duration_bfs = std::chrono::duration_cast<std::chrono::milliseconds>(end_bfs - start_bfs).count();
-        cout << setw(12) << (to_string(duration_bfs) + "ms") << setw(12) << num_tries;
+        cout << setw(12) << (to_string(duration_bfs) + "ms") 
+             << setw(12) << path.size()  // Número de movimentos BFS
+             << setw(12) << num_tries;
 
         // DFS
         path.clear(); num_tries = 0;
@@ -61,7 +67,9 @@ int main() {
         solver.dfs(copy, path, &num_tries); 
         auto end_dfs = std::chrono::high_resolution_clock::now();
         auto duration_dfs = std::chrono::duration_cast<std::chrono::milliseconds>(end_dfs - start_dfs).count();
-        cout << setw(12) << (to_string(duration_dfs) + "ms") << setw(12) << num_tries;
+        cout << setw(12) << (to_string(duration_dfs) + "ms") 
+             << setw(12) << path.size()  // Número de movimentos DFS
+             << setw(12) << num_tries;
         
         // A*
         path.clear(); num_tries = 0;
@@ -69,7 +77,9 @@ int main() {
         solver.A_star(copy, path, &num_tries, -1); 
         auto end_astar = std::chrono::high_resolution_clock::now();
         auto duration_astar = std::chrono::duration_cast<std::chrono::milliseconds>(end_astar - start_astar).count();
-        cout << setw(12) << (to_string(duration_astar) + "ms") << setw(12) << num_tries << endl;
+        cout << setw(12) << (to_string(duration_astar) + "ms") 
+             << setw(12) << path.size()  // Número de movimentos A*
+             << setw(12) << num_tries << endl;
 
         index++;
     }
